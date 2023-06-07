d92017b872667a3903018d9f4a70db4e493ca85a
juniooor <juniormodern@hotmail.com>
2023-06-06 22:06:12 -0300

    criando models livros/autores/categorias

--- livraria/livros/models.py
+++ livraria/livros/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from autores.models import Autor
+from categorias.models import Categoria
 
-# Create your models here.
+class Livros(models.Model):
+    nome = models.CharField(max_length=120)
+    autorid = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    categoriaid = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.nome
