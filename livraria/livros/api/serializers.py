edc70976bba9aeed99251a833b31cd72f2bc7d5d
juniooor <juniormodern@hotmail.com>
2023-06-07 21:23:14 -0300

    edit models

--- livraria/livros/api/serializers.py
+++ livraria/livros/api/serializers.py
@@ -6,4 +6,4 @@ from livros.models import Livros
 class LivroSerializers(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Livros
-        fields = ['nome', 'autorid', 'categoriaid', 'id']
+        fields = ['nome', 'autores', 'categorias', 'id']
--- livraria/livros/models.py
+++ livraria/livros/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from autores.models import Autor
 from categorias.models import Categoria
 
+
 class Livros(models.Model):
     nome = models.CharField(max_length=120)
-    autorid = models.ForeignKey(Autor, on_delete=models.CASCADE)
-    categoriaid = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    autores = models.ManyToManyField(Autor)
+    categorias = models.ManyToManyField(Categoria)
     
     def __str__(self):
         return self.nome
